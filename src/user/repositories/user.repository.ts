bb66c00080982024cb0b749074927dc2b5542a87
Ronald J Costa <ronald.juscosta@gmail.com>
2023-04-06 18:50:29 -0300

    fix: add env in gitignore

---
+++ src/user/repositories/user.repository.ts
@@ -0,0 +1,12 @@
+import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
+
+@Injectable()
+export class UserRepository {
+  constructor(private readonly prisma: PrismaService) { }
+
+  async findAll() {
+    return this.prisma.user.findMany();
+  }
+
+}
